8bc16ae1b906cb24d1774f8db9db6d409a65d7c2
King Active <144297074+King-Active@users.noreply.github.com>
2025-04-14 20:02:45 +0800

    Update gated_layer.py

--- modules/gated_layer.py
+++ modules/gated_layer.py
@@ -15,7 +15,6 @@ class GatedLayer(nn.Module):
             nn.SiLU(),
             ModConv(self.cond, nn.Conv1d(H * 2, H * 2, 1)),
         )
-        
         self.gru = nn.Sequential(
             ModConv(self.cond, nn.Conv1d(H, H, 3, 1, 1, groups=H)),
             nn.SiLU(),
@@ -29,6 +28,6 @@ class GatedLayer(nn.Module):
             :param X:   噪声图谱  (B, h_dim, L)
             :param A:   经过 label 和 t 调制后的音频 (B, A, L)
         """
-        h, g = self.exp(torch.cat([X, A], dim=1)).chunk(2, dim=1)
+        h, g = self.exp(torch.cat([X, A], dim=1))
         h = self.gru(h) * F.silu(g) + self.gru(g) * F.silu(h)
         return self.out(h)
