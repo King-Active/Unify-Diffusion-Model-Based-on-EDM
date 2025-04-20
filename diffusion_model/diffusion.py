e524f61b97f526258bbb9ff7c8c923fd57ca8c0a
King Active <144297074+King-Active@users.noreply.github.com>
2025-04-21 00:22:56 +0800

    Update diffusion.py

--- diffusion_model/diffusion.py
+++ diffusion_model/diffusion.py
@@ -40,14 +40,14 @@ class UnifyDiffusionFramework:
             elif mode.model == 'VP':
                 beta_d = torch.tensor(19.9, device=d)
                 beta_min = torch.tensor(0.1, device=d)
-                return torch.sqrt(torch.exp(0.5 * beta_d * (t ** 2) + beta_min * t ) - 1)
+                return ((0.5 * beta_d * (t ** 2) + beta_min * t ).exp() - 1).sqrt()
 
             elif mode.model == 'VE':
-                sigma_min = torch.tensor(0.02,device=d)
-                sigma_max = torch.tensor(100.,device=d)
+                # sigma_min = torch.tensor(0.02,device=d)
+                # sigma_max = torch.tensor(100.,device=d)
 
                 # below is the original formula of VP noise schedule function both in sampling and training
-                return sigma_min * (sigma_max / sigma_min)**t
+                # return sigma_min * (sigma_max / sigma_min)**t
 
                 # below is the formula given by EDM through ODE inference in sampling (training is the same as above)
                 return torch.sqrt(t)
@@ -58,7 +58,7 @@ class UnifyDiffusionFramework:
             # 不同 B 采用不同训练调度采样值
             if mode.sche == 'EDM':
                 """
-                采用基于 sigma 的均匀分布调度 
+                采用基于 sigma 的均匀分布调度
                 """
                 P_mean = -1.2
                 P_std = 1.2
@@ -84,10 +84,10 @@ class UnifyDiffusionFramework:
 
             elif mode.sche == 'VE':
                 """
-                采用基于 sigma 的均匀分布调度 
+                采用基于 sigma 的均匀分布调度
                 """
                 sigma_min = torch.tensor(0.02, device=d)
-                sigma_max = torch.tensor(50.0, device=d)
+                sigma_max = torch.tensor(100.0, device=d)
                 ln_sigma_min = torch.log(sigma_min)
                 ln_sigma_max = torch.log(sigma_max)
                 ln_sigma = ln_sigma_min + (ln_sigma_max - ln_sigma_min) * torch.rand(B, 1, 1, device=d)
@@ -118,9 +118,9 @@ class UnifyDiffusionFramework:
                 return t, sigma_func(t)
 
             elif mode.sche == 'VE':
-                sigma_max = torch.tensor(50.0, device=d)
+                sigma_max = torch.tensor(100.0, device=d)
                 sigma_min = torch.tensor(0.02, device=d)
-                t = sigma_max  **  2 * (sigma_min  **  2 / sigma_max  **  2)  **  s
+                t = (sigma_max  **  2) * (sigma_min  **  2 / sigma_max  **  2)  **  s
                 return t, sigma_func(t)
 
         self.sigma_sche_train = sigma_sche_train_fun
@@ -130,7 +130,6 @@ class UnifyDiffusionFramework:
         self.mode = mode
 
     def d_model(self, model: Denoiser, x_hat: X, sigma: T, t):
-        d = x_hat.device
 
         c_skip, c_out, c_in, c_noise  =  None, None, None, None
 
@@ -191,119 +190,119 @@ class UnifyDiffusionFramework:
 
     @torch.no_grad()
     def sample( self, denoiser: Denoiser, num_steps, x_n: X ):
-
-        #  x_n 随机初始化噪声
-        B = x_n.size(0)
+        """
+            Adapt from https://github.com/NVlabs/edm/blob/main/generate.py
+        """
         d = x_n.device
+        B = x_n.shape[0]
 
-        # 所有 Batch 共享
-        ts, sigmas = self.sigma_sche_sample(num_steps)
-        sigmas = torch.tensor([*sigmas.tolist(), 0], device=d)
-        sigmas = sigmas[:, None, None, None].repeat(1, B, 1, 1)
-        loop = zip(sigmas[:-1], sigmas[1:])         # decrease
+        beta_min = 0.1
+        beta_d = 19.9
+        rho = 7
+        epsilon_s=0.003
 
-        def dx_dt_edm(x_hat: X, sigma: T):
-            x_0 = self.inferring_sample(denoiser, x_hat, sigma, sigma)
-            # point to high probability direction
-            return (x_hat - x_0) / sigma  # 指向 噪声
+        S_churn = 40.
+        S_min = 1e-1
+        S_max = 1e1
+        S_noise = 1.003
 
-        if self.mode.model == 'EDM' and self.mode.flow == 'SS':
-            """
-                EDM风格随机性采样：对比一阶 Euler 和 二阶 Heun
-                不同于 欧拉丸山 离散化方法，其严格遵循 SDE 反向，先降噪后加噪
-                EDM风格先加噪，后遵循 ODE 风格的降噪
-            """
-
-            # x_cur = x_n * (sigmas[0, 0, 0, 0] ** 2 + self.sigma_data ** 2) ** .5
-            x_cur = x_n * sigmas[0, 0, 0, 0]
-
-            for t_cur, t_nxt in loop:
-                # t_nxt < t_cur
-                # 避免细节缺失 及 过饱和
-                S_churn = 40.
-                S_tmin = 1e-1
-                S_tmax = 1e1
-                S_noise = 1.003
-
-                gamma = min(S_churn / num_steps, 2 ** .5 - 1) if S_tmin <= t_cur[0, 0, 0] <= S_tmax else 0
-                t_hat = t_cur + gamma * t_cur   # time nosie
-                x_hat = x_cur + torch.sqrt(t_hat ** 2 - t_cur ** 2) * S_noise * torch.randn_like(x_cur)     # chart noise
-
-                # Euler
-                d_i = dx_dt_edm(x_hat, t_hat)                   # 指向噪声
-                x_nxt = x_hat + (t_nxt - t_hat) * d_i       # 反向： 指向纯净
-
-                # Heun
-                if self.mode.diff == "Heun" and t_nxt[0, 0, 0] > 0:
-                    d_i_ = dx_dt_edm(x_nxt, t_nxt)
-                    x_nxt = x_hat + (t_nxt - t_hat) * 0.5 * (d_i + d_i_)
-
-                x_cur = x_nxt
-
-            return x_cur
-
-        elif self.mode.model == 'EDM' and self.mode.flow == 'PFODE':
-            """
-                通式
-                EDM风格确定性采样：对比一阶 Euler 和 二阶 Heun
-                通过适配 s(t) sigma(t) 可实现 VP VE 在本函数下的确定性采样
-            """
-
-            # x_cur = x_n * (sigmas[0, 0, 0, 0] ** 2 + self.sigma_data ** 2) ** .5
-            x_cur = x_n * sigmas[0, 0, 0, 0]
-
-            for t_cur, t_nxt in loop:
-                # t_nxt < t_cur
-                d_i = dx_dt_edm(x_cur, t_cur)                   # 指向噪声
-
-                # Euler
-                x_nxt = x_cur + (t_nxt - t_cur) * d_i       # 反向： 指向纯净
-
-                # Heun
-                if self.mode.diff == "Heun" and t_nxt[0, 0, 0] > 0:
-                    d_i_ = dx_dt_edm(x_nxt, t_nxt)
-                    x_nxt = x_cur + (t_nxt - t_cur) * 0.5 * (d_i + d_i_)
-
-                x_cur = x_nxt
-
-            return x_cur
-
-        elif self.mode.model == 'VP':
-            """
-                VP风格随机采样
-                欧拉丸山风格，先利用SDE降噪，再添加随机项
-            """
-
-            x_t = x_n * sigmas[0, 0, 0, 0]
-
-            for sigma_t, t in zip(sigmas, ts):
-                s_t = 1 / torch.sqrt(1 + sigma_t ** 2)      # scale
-                beta_max = 20.0
-                beta_min = 0.1
-                beta_t = beta_min + (beta_max-beta_min) * t
-
-                pred = self.inferring_sample(denoiser, x_t/s_t, sigma_t, t)
-                determine = (1 / torch.sqrt(1-beta_t)) * (x_t + beta_t * ( (pred - x_t/s_t) / s_t * (sigma_t**2) ) )
-                random = torch.sqrt(beta_t) * torch.randn_like(x_t)
-
-                x_t = determine + random
-
-            return x_t
-
+        # Unify time steps
+        sigma_min = {'VP': None, 'VE': 0.02, 'EDM': 0.002}[self.mode.model]
+        sigma_max = {'VP': None, 'VE': 100, 'EDM': 80}[self.mode.model]
+        steps = torch.arange(num_steps, device=d)
+        if self.mode.model == 'VP':
+            t_steps = 1 + steps / (num_steps - 1) * (epsilon_s - 1)
         elif self.mode.model == 'VE':
-            """
-                VE风格随机采样
-                欧拉丸山风格，先利用SDE降噪，再添加随机项
-            """
+            t_steps = (sigma_max ** 2) * ((sigma_min ** 2 / sigma_max ** 2) ** (steps / (num_steps - 1)))
+        elif self.mode.model == 'EDM':
+            t_steps = (sigma_max ** (1 / rho) + steps / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
+        else:
+            t_steps = steps /  num_steps
+            t_steps = 0.5 + (t_steps - 0.5) * (1 - (5e-2) * 2)
+            log_snr = (5e-2) * torch.sign(0.5 - t_steps) * torch.log(1 - 2 * torch.abs(0.5 - t_steps))
+            t_steps = 0.67 * torch.exp(-0.5 * log_snr)
 
-            x_t = x_n * sigmas[0, 0, 0, 0]
+        t_steps = torch.cat([t_steps, torch.zeros_like(t_steps[:1])])
 
-            for sigma_cur, sigma_pre in loop:
+        # Unify Sigma(t)
+        if self.mode.sche == 'VP':
+            sigma = lambda t: (torch.e ** (0.5 * beta_d * (t ** 2) + beta_min * t) - 1) ** 0.5
+            sigma_deriv = lambda t: 0.5 * (beta_min + beta_d * t) * (sigma(t) + 1 / sigma(t))
+            sigma_inv = lambda sigma: ((beta_min ** 2 + 2 * beta_d * (sigma ** 2 + 1).log()).sqrt() - beta_min) / beta_d
+        elif self.mode.sche == 'VE':
+            sigma = lambda t: t.sqrt()
+            sigma_deriv = lambda t: 0.5 / t.sqrt()
+            sigma_inv = lambda sigma: sigma ** 2
+        elif self.mode.sche == "EDM":
+            sigma = lambda t: t
+            sigma_deriv = lambda t: 1
+            sigma_inv = lambda sigma: sigma
 
-                pred = self.inferring_sample(denoiser, x_t, sigma_cur, 0)
-                determine = x_t - ( (sigma_cur**2 - sigma_pre**2) / sigma_cur**2 ) * (pred - x_t)
-                random = torch.sqrt(sigma_cur**2 - sigma_pre**2) * torch.randn_like(x_t)
+        # Unify S(t)
+        if self.mode.model == 'VP':
+            s = lambda t: 1 / (1 + sigma(t) ** 2).sqrt()
+            s_deriv = lambda t: -sigma(t) * sigma_deriv(t) * (s(t) ** 3)
+        else:
+            s = lambda t: 1
+            s_deriv = lambda t: 0
 
-                x_t = determine + random
+        # Unify Sample Process
+        t_next = t_steps[0]
+        x_next = x_n * sigma(t_next) * s(t_next)        # x_hat * s --> original x
 
-            return x_t
+        def dx_dt(x_hat, t_hat):
+            pred = self.inferring_sample(denoiser, x_hat / s(t_hat), sigma(t_hat).view(B, 1, 1), t_hat.view(B, 1, 1))
+            dxdt = (sigma_deriv(t_hat) / sigma(t_hat) + s_deriv(t_hat) / s(t_hat)) * x_hat - sigma_deriv(t_hat) * s(
+                t_hat) / sigma(t_hat) * pred
+            return dxdt
+
+        if self.mode.flow == 'SDE':
+            for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])):
+                x_cur = x_next
+
+                # Only adding extra noise in limited range of sigma
+                gamma = min(S_churn / num_steps, 2**0.5 - 1) if S_min <= sigma(t_cur) <= S_max else 0
+                # back to pre-noise level
+                sigma_new = sigma(t_cur) + gamma * sigma(t_cur)
+                # get the timestamp related to pre-noise level
+                t_hat = sigma_inv(sigma_new)
+                # 1 - update the scale s(t_cur)
+                # 2 - add the noise
+                x_hat = ((x_cur/s(t_cur)) * s(t_hat) +
+                             S_noise * torch.randn_like(x_cur) *                                # normalized noise with standard deviation 'S_noise'
+                             ( sigma(t_hat) ** 2 - sigma(t_cur) ** 2 ).clip(min=0).sqrt() *     # coefficient
+                             s(t_hat) )                                                         # scaling
+
+                # Euler step
+                d_0 = dx_dt(x_hat, t_hat)
+                # x_new <- dx + x_hat
+                x_mid = x_hat + (t_next - t_hat) * d_0
+
+                # Heun Step
+                if self.mode.diff == 'Euler' or i == num_steps - 1:
+                    x_next = x_mid
+                else:
+                    assert self.mode.diff == 'Heun'
+                    d_1 = dx_dt(x_mid, t_next)
+                    x_next = x_hat + (t_next - t_hat) * 0.5 * (d_0 + d_1)
+
+        else:
+            assert self.mode.flow == 'PFODE'
+
+            for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])):
+                x_cur = x_next
+
+                # Euler step
+                d_0 = dx_dt(x_cur, t_cur)
+                # x_new <- dx + x_hat
+                x_mid = x_cur + (t_next - t_cur) * d_0
+
+                # Heun Step
+                if self.mode.diff == 'Euler' or i == num_steps - 1:
+                    x_next = x_mid
+                else:
+                    assert self.mode.diff == 'Heun'
+                    d_1 = dx_dt(x_mid, t_next)
+                    x_next = x_cur + (t_next - t_cur) * 0.5 * (d_0 + d_1)
+
+        return x_next
